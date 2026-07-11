f156b2fb7f28d6118b89b9899e88d9b6f6e5d07e
JaviMarinn <emmanuelperezmarin2207@gmail.com>
2026-07-10 23:36:06 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+const menuToggle = document.querySelector('.menu-toggle');
+const nav = document.querySelector('.site-nav');
+
+if (menuToggle && nav) {
+  menuToggle.addEventListener('click', () => {
+    nav.classList.toggle('open');
+  });
+}
