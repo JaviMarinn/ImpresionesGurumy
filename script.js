4a103de521c0b9e43dcf8da84af3bb51a8343d59
JaviMarinn <emmanuelperezmarin2207@gmail.com>
2026-07-14 14:21:42 -0500

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-const menuToggle = document.querySelector('.menu-toggle');
-const nav = document.querySelector('.site-nav');
-
-if (menuToggle && nav) {
-  menuToggle.addEventListener('click', () => {
-    nav.classList.toggle('open');
-  });
-}
